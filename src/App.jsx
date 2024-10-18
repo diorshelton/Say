d4a69d090dcaa9f7c9662c1b6e3e7a9ada77c96f
Dior.Shelton <dior.shelton@gmail.com>
2024-10-18 15:25:48 -0400

    Remove commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import "./App.css";
 import Button from "./components/Button";
-// import Form from "./components/Form";
 
 function App() {
 	return (
@@ -10,7 +9,6 @@ function App() {
 			<div>
 				<Button>Sign in</Button>
 				<Button>Register</Button>
-				{/* <Form></Form> */}
 			</div>
 		</>
 	);
